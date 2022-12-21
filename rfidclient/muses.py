9526c6605ecbd59ef00bb18b64b7dfa8df10b0c3
Owen Holloway <owen.holloway@lanceolata.com.au>
2022-12-21 18:17:49 +1100

    script recovers on it own

--- rfidclient/muses.py
+++ rfidclient/muses.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
-
 import asyncio
 import serial
 import pyautogui
 import findserial
 
-ports = findserial.getPorts()
-
 serial_port = ""
+found_port = False
 
-for port, desc, hwid in sorted(ports):
-    print("{}: {} [{}]".format(port, desc, hwid))
-    if "USB to UART Bridge Controller" in desc :
-        serial_port = port
-        break
-    if "USB2.0-Serial" in desc:
-        serial_port = port 
-        break       
-
-if len(serial_port) < 2:
-    print("No Device Found")
-    exit()
-else:
-    print("Using {}".format(serial_port))
-
-
-# Set the serial port and baud rate
-ser = serial.Serial(serial_port, 9600)
+def resolvePort():
+    ports = findserial.getPorts()
+    for port, desc, hwid in sorted(ports):
+        #print("{}: {} [{}]".format(port, desc, hwid))
+        if "USB to UART Bridge Controller" in desc :
+            return port
+        if "USB2.0-Serial" in desc:
+            return port
+    return ""
 
 async def processData(data):
+    print(data)
     val_pair = str(data).split(";")
     if len(val_pair) > 1:
         if str(val_pair[0]).__contains__("UID.HEX"):
@@ -38,5 +28,17 @@ async def processData(data):
 
 # Continuously read serial data and emulate keyboard input
 while True:
-    data = ser.readline()
-    asyncio.run(processData(data))
+    if found_port:
+        # Set the serial port and baud rate
+        try:
+            data = ser.readline()
+            asyncio.run(processData(data))
+        except:
+            found_port = False
+            serial_port = ""
+    else:
+        serial_port = resolvePort()
+        if len(serial_port) > 2:
+            print("{}".format(serial_port))
+            found_port = True
+            ser = serial.Serial(serial_port, 9600)
