8504e386b718d76fbd006b523c242da841f7d4ef
Tarran Raichert <tarran96@gmail.com>
2022-12-27 15:58:00 +1100

    Add 3D print files Fix board definition for ESP32

--- rfidreader/src/board.h
+++ rfidreader/src/board.h
@@ -7,7 +7,7 @@ const char* MCU_ARCH = "esp8266";
 
 #if az_delivery_devkit_v4
 
-const char* MCU_TYPE = "LOLin_ESP8266_NodeMCU_v3.0";
-const char* MCU_ARCH = "esp8266";
+const char* MCU_TYPE = "Duinotech_ESP32";
+const char* MCU_ARCH = "esp32";
 
 #endif
